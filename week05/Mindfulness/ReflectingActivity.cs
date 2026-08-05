e6321e389798de9526619fbaf041f4005d409e48
edbaez-dev <edu94baez@gmail.com>
2026-08-05 06:26:30 -0300

    w05 Mindfulness Desing Assignement

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public ReflectingActivity()
+    {
+    }
+
+    public void Run()
+    {
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+    }
+
+    public void DisplayQuestions()
+    {
+    }
+}
